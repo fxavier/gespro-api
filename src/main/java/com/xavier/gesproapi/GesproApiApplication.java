7409834ba50c64d41915de95355f87137671a495
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-11-17 23:01:50 +0000

    first commit

---
+++ src/main/java/com/xavier/gesproapi/GesproApiApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.gesproapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesproApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesproApiApplication.class, args);
+    }
+
+}
